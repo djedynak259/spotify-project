78f87c4f9782126de5adc6cf7aea43f6db9fd63d
djedynak259 <dajed@outlook.com>
2017-10-25 17:14:28 -0700

    commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,52 @@
+var request = require('request'); // "Request" library
+
+var client_id = '13ff1a4fe1a743269c9153a2c3af1dea'; 
+var client_secret = 'fbf7f9a8b97a4f299db06d298e352e85'; 
+
+// your application requests authorization
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+
+    // use the access token to access the Spotify Web API
+    var token = body.access_token;
+    var options = {
+      url: 'https://api.spotify.com/v1/search?q=daft+punk&type=album',
+      headers: {
+        'Authorization': 'Bearer ' + token
+      },
+      json: true
+    };
+    request.get(options, function(error, response, body) {
+    	let test = body.albums.items.map(e=>e.name)
+    		console.log(test)
+    })
+  }
+});
+
+
+
+
+let base = 'https://api.spotify.com/';
+let albumSearch='v1/search?q=michael+jackson&type=album'
+
+// fetch(base+albumSearch).then(response=>response.json()).then(resp=>console.log(resp))
+
+// fetch('https://accounts.spotify.com/api/token').then(response=>response.json()).then(resp=>console.log(resp))
+
+
+
+
 
 
 // function search(){
