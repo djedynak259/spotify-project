dfd6118f02fd0940ace3148d0f8dee48ce4b1ef7
djedynak259 <dajed@outlook.com>
2017-10-26 21:55:22 -0700

    next step

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
-var request = require('request');
-var rp = require('request-promise'); 
-var _ = require('lodash');
+let request = require('request');
+let rp = require('request-promise'); 
+let _ = require('lodash');
 
-var client_id = '13ff1a4fe1a743269c9153a2c3af1dea'; 
-var client_secret = 'fbf7f9a8b97a4f299db06d298e352e85'; 
+let client_id = '13ff1a4fe1a743269c9153a2c3af1dea'; 
+let client_secret = 'fbf7f9a8b97a4f299db06d298e352e85'; 
+
+let artistOfInterest = {
+	name: 'Daft Punk',
+	id:'4tZwfgrHOc3mvqYlEYSvVi'
+}
 
 // your application requests authorization
-var authOptions = {
+let authOptions = {
   url: 'https://accounts.spotify.com/api/token',
   headers: {
     'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
@@ -33,11 +38,11 @@ let auth = rp.post(authOptions)
 
 
 // get Genres
-let getGenres = function(artistId){
+let getGenres = function(artistOfInterest){
 	return function(body){
-		var token = body.access_token;
-		var options = {
-		  url: `https://api.spotify.com/v1/artists/${artistId}`,
+		let token = body.access_token;
+		let options = {
+		  url: `https://api.spotify.com/v1/artists/${artistOfInterest.id}`,
 		  headers: {
 		    'Authorization': 'Bearer ' + token
 		  },
@@ -46,80 +51,146 @@ let getGenres = function(artistId){
 		return rp(options).then(e=>{
 			console.log(e.genres)
 			return {
-				access_token:body.access_token, 
+				access_token:body.access_token,
+				artistOfInterest: artistOfInterest,
 				genres:	e.genres			
 			}
 		}).catch(errorHandle)
 	}
 }	
 
-
-let example = function(body){
-	var token = body.access_token;
-	var options = {
-	  url: 'https://api.spotify.com/v1/search?q=tania%20bowra&type=artist&limit=50',
+let findSimilarArtists = function(body){
+	let token = body.access_token;
+	let options = {
+	  url: 'https://api.spotify.com/v1/artists/4tZwfgrHOc3mvqYlEYSvVi/related-artists?limit=50',
 	  headers: {
 	    'Authorization': 'Bearer ' + token
 	  },
 	  json: true
 	};
-	return rp(options).then(e=>console.log(e)).catch(errorHandle)
+
+	return rp(options).then(function(data){
+		let sortedResults = _.sortBy(data.artists, [e=>_.intersection(body.genres, e.genres).length, 'popularity'])
+		.filter(e=>{
+				return _.intersection(body.genres, e.genres).length > 0
+		})
+		.reverse()
+
+		let resultsToLog = sortedResults.map(e=>{
+			return{
+				name: e.name,
+				genres:_.intersection(body.genres, e.genres),
+				popularity: e.popularity
+			}
+		})
+
+		let resultsToReturn = sortedResults.map(e=>{
+			return{
+				id:e.id,
+				artistName: e.name,
+			}
+		})
+
+		console.log(`Artists related to ${body.artistOfInterest.name} sorted by similarity and then popularity`, resultsToLog)
+
+		return {
+			access_token: token,
+			artists:resultsToReturn
+		}
+	})
+}
+
+// findPopularAlbums from Artist
+
+let findArtistAlbumsIds = function(body){
+	let promiseArr=[]
+
+	let token = body.access_token;
+	let commonOptions = {
+	  headers: {
+	    'Authorization': 'Bearer ' + token
+	  },
+	  json: true
+	};
+
+	body.artists.forEach(e=>{
+		let artistOption = {
+			url: `https://api.spotify.com/v1/artists/${e.id}/albums?market=US&limit=50`,
+			headers:commonOptions.headers,
+			json: commonOptions.json
+		}
+
+		let findAlbumsPromise = rp(artistOption).then(data=>{
+			return {
+				artist:e.artistName,
+				albums:data.items.map(j=>j.id)
+			}
+		})
+		promiseArr.push(findAlbumsPromise)
+	})
+
+	return Promise.all(promiseArr).then(data=>{
+		return {
+				access_token: token,
+				artistAlbums: data
+		}
+	})
+
+}
+
+// Find Top Three Albums by Popularity
+
+let topThreeAlbumsByPopularity = function(body){
+	let token = body.access_token;
+	let commonOptions = {
+	  headers: {
+	    'Authorization': 'Bearer ' + token
+	  },
+	  json: true
+	};
+
+	body.artistAlbums.forEach(e=>{
+		let albumOptions = {
+			url: `hhttps://api.spotify.com/v1/albums/${id}`,
+			headers:commonOptions.headers,
+			json: commonOptions.json
+		}
+
+		rp(albumOptions)
+		.then()
+		.then()
+		.then(e=>console.log(e)).catch(errorHandle)
+	})
 }
 
 
-let findSimilar = function(body){
-	
-		var token = body.access_token;
-		var options = {
-		  url: 'https://api.spotify.com/v1/artists/4tZwfgrHOc3mvqYlEYSvVi/related-artists',
-		  headers: {
-		    'Authorization': 'Bearer ' + token
-		  },
-		  json: true
-		};
+auth.then(getGenres(artistOfInterest))
+	.then(findSimilarArtists)
+	.then(findArtistAlbumsIds)
+	// .then(topThreeAlbumsByPopularity)
+	.then(e=>console.log('done',e))
 
-		function similarTags(genresToFind, currentGenres){
-			let result = false
-			genresToFind.forEach(e=>{
-				if(currentGenres.includes(e)){
-					result = true
-				}
-			})
-			return true
-		}
 
-		return rp(options).then(function(data){
-			let sortedResults = _.sortBy(data.artists, [e=>_.intersection(body.genres, e.genres).length, 'popularity'])
-			return sortedResults
-			.filter(e=>{
-					return _.intersection(body.genres, e.genres).length > 0
-			})
-			.reverse()
-			.map(e=>{
-				return{
-					name:e.name,
-					genres:_.intersection(body.genres, e.genres),
-					popularity:e.popularity
-				}
-			})
-		})
-	}
+// let example = function(body){
+// 	let token = body.access_token;
+// 	let options = {
+// 	  url: 'https://api.spotify.com/v1/search?q=tania%20bowra&type=artist&limit=50',
+// 	  headers: {
+// 	    'Authorization': 'Bearer ' + token
+// 	  },
+// 	  json: true
+// 	};
+// 	return rp(options).then(e=>console.log(e)).catch(errorHandle)
 // }
 
-
-
-
-auth.then(getGenres('4tZwfgrHOc3mvqYlEYSvVi'))
-	.then(findSimilar)
-	.then(e=>console.log(e))
 // auth.then(example)
 
 
 // request.post(authOptions, function(error, response, body) {
 //   if (!error && response.statusCode === 200) {
 //     // use the access token to access the Spotify Web API
-//     var token = body.access_token;
-//     var options = {
+//     let token = body.access_token;
+//     let options = {
 //       url: 'https://api.spotify.com/v1/artists/4tZwfgrHOc3mvqYlEYSvVi',
 //       headers: {
 //         'Authorization': 'Bearer ' + token
